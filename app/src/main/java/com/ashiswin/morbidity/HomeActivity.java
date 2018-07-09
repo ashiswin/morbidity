55c14370062cdd87f324aa61785f9ef81e9fdbb2
Isaac Ashwin <ashiswin@gmail.com>
2018-07-09 08:00:01 +0800

    Fixed app icon background. Added preference icons.

--- app/src/main/java/com/ashiswin/morbidity/HomeActivity.java
+++ app/src/main/java/com/ashiswin/morbidity/HomeActivity.java
@@ -2,9 +2,12 @@ package com.ashiswin.morbidity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.PorterDuff;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
@@ -46,6 +49,7 @@ public class HomeActivity extends AppCompatActivity {
         getSupportActionBar().setElevation(0);
         centerTitle();
         getSupportActionBar().setTitle("TIMER");
+
 
         preferences = PreferenceManager.getDefaultSharedPreferences(HomeActivity.this);
 
